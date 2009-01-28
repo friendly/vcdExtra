6d912fc877ffd88ee691c4615ed921eb176eb5f6
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-28 20:41:02 +0000

    Added new data, functions & NAMESPACE
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vcdextra/pkg@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/collapse.table.R
@@ -0,0 +1,27 @@
+# collapse a contingency table or ftable by re-assigning levels of table variables
+
+collapse.table <- function(table, ...) {
+	nargs <- length(args <- list(...))
+	if (!nargs) 
+		return(table)
+	if (inherits(table, "ftable"))
+		table <- as.table(table)
+	if (inherits(table, "table")) {
+		tvars <- names(dimnames(table))
+		table <- as.data.frame.table(table)
+		freq <- table[,"Freq"]
+	}
+	else stop("Argument must be a table or ftable object")
+	
+	names <- names(args)
+	for (i in 1:nargs) {
+		vals <- args[[i]]
+		nm <- names[[i]]
+		if(any(nm==tvars)) levels(table[[nm]]) <- vals
+		else warning(nm, " is not among the table variables.")
+	}
+#    term <- paste(tvars, collapse = '+')
+#    form <- as.formula(paste("freq ~", term))
+#    cat("term: ", term, "\n")
+	xtabs(as.formula(paste("freq ~", paste(tvars, collapse = '+'))), data=table)
+}
