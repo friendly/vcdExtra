0d65978839773d177d46b1aa2789362e21603b4c
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-06 21:11:36 +0000

    Fixed bugs in mosaic.glm and mosaic.gnm
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vcdextra/pkg@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/mosaic.glm.R
+++ R/mosaic.glm.R
@@ -1,3 +1,10 @@
+# mosaic plot for a glm object
+#  Allow it to use residuals of any type computed by residuals()
+#  or to pass residuals calculated by another function, e.g., rstandard(), rstudent()
+# 
+#  Allow to apply to any model with discrete factors
+# last modified: 2/6/2009 2:45PM
+#  - fixed buggy version using ideas from vcd:::plot.loglm
 
 ## TODO: move to utility.R
 is.discrete.model <- function(model)
@@ -5,30 +12,39 @@ is.discrete.model <- function(model)
 
 
 `mosaic.glm` <-
-function(x, residuals=NULL, ..., residuals_type="deviance") {
-  require(vcd)
+		function(x, panel=mosaic, type=c("observed", "expected"), residuals=NULL, 
+				residuals_type = c("pearson", "deviance", "rstandard"), gp = shading_hcl, 
+				gp_args = list(), ...) 
+{
+	
+	require(vcd)
 	if (!inherits(x,"glm")) stop("mosaic.glm requires a glm object")
+	# this should work for any glm family, where all predictors are factors
+	# But maybe not sensible for non-count data??
 	if (!is.discrete.model(x)) stop("only factors are allowed")
-
-# Allow it to work for any family, where all predictors are factors
-# -- how to test this?
-#	if (x$family$family != "poisson") stop("mosaic.glm requires a poisson family glm")
 	
 	xlevels <- x$xlevels
 	df.residual <- x$df.residual
-	data <- x$data
+	observed <- x$data
 	# if a data.frame, extract the frequencies, re-shape as a table
-  if (inherits(data, "data.frame")) {
-    data <- array(x$y, dim=lapply(xlevels,length), dimnames=xlevels) 
-  }
-
-  if (missing(residuals))
-	  residuals <- residuals(x, type=residuals_type)
-  # reshape the residuals to conform to the structure of data
-  residuals <- array(residuals, dim=lapply(xlevels,length), dimnames=xlevels) 
-
-#  Problem:  to pass the df= argument via gp_args, we suppress any user-supplied other gp_args supplied through dots
-#  Solution: ???
-  mosaic(data, residuals=residuals, residuals_type=residuals_type, gp_args=list(df=df.residual), ...)
+	if (inherits(observed, "data.frame")) {
+		observed <- array(x$y, dim=lapply(xlevels,length), dimnames=xlevels) 
+	}
+	expected <- array(fitted(x), dim=lapply(xlevels,length), dimnames=xlevels) 
+	
+	residuals_type <- match.arg(tolower(residuals_type), c("pearson", "deviance", "rstandard"))
+	if (missing(residuals))
+		residuals <- if (residuals_type=="rstandard") rstandard(x)
+				else residuals(x, type=residuals_type)
+	# reshape the residuals to conform to the structure of data
+	residuals <- array(residuals, dim=lapply(xlevels,length), dimnames=xlevels) 
+	
+	gp <- if (inherits(gp, "grapcon_generator")) 
+				do.call("gp", c(list(observed, residuals, expected, x$df.residual), 
+								as.list(gp_args)))
+			else gp
+	
+	panel(observed, residuals=residuals, expected=expected, type=type,
+			residuals_type=residuals_type, gp=gp, ...)
 }
 
--- R/mosaic.gnm.R
+++ R/mosaic.gnm.R
@@ -1,28 +1,44 @@
+## TODO: fix residuals_type for gnm objects
+
 `mosaic.gnm` <-
-function(x,  residuals=NULL, ..., residuals_type="deviance") {
+		function(x, panel=mosaic, type=c("observed", "expected"), residuals=NULL, 
+				residuals_type = c("pearson", "deviance", "rstandard"), gp = shading_hcl, 
+				gp_args = list(), ...) 
+{
+	
+	require(vcd)
+	
 	if (!inherits(x,"gnm")) stop("mosaic.gnm requires a gnm object")
 	if (!is.discrete.model(x)) stop("only factors are allowed")
 	
 	xlevels <- x$xlevels
 	df.residual <- x$df.residual
-	data <- x$data
+	observed <- x$data
 	# if a data.frame, extract the frequencies, re-shape as a table
-  if (inherits(data, "data.frame")) {
-    data <- array(x$y, dim=lapply(xlevels,length), dimnames=xlevels) 
-  }
-
-  if (missing(residuals))
-	  residuals <- residuals(x, type=residuals_type)
-
-  # reshape the residuals to conform to the structure of data
-  if (!inherits(residuals, "table") && !is.null(x$table.attr)) {
-        attributes(residuals) <- x$table.attr
-  } 
-  else {
-        residuals <- array(residuals, dim=lapply(xlevels,length), dimnames=xlevels) 
-  }
- 
-  mosaic(data, residuals=residuals, residuals_type=residuals_type, gp_args=list(df=df.residual), ...)
-
+	if (inherits(observed, "data.frame")) {
+		observed <- array(x$y, dim=lapply(xlevels,length), dimnames=xlevels) 
+	}
+	expected <- array(fitted(x), dim=lapply(xlevels,length), dimnames=xlevels) 
+	
+	
+	if (missing(residuals))
+		residuals <- residuals(x, type=residuals_type)
+	
+	# reshape the residuals to conform to the structure of data
+	if (!inherits(residuals, "table") && !is.null(x$table.attr)) {
+		attributes(residuals) <- x$table.attr
+	} 
+	else {
+		residuals <- array(residuals, dim=lapply(xlevels,length), dimnames=xlevels) 
+	}
+	
+	gp <- if (inherits(gp, "grapcon_generator")) 
+				do.call("gp", c(list(observed, residuals, expected, x$df.residual), 
+								as.list(gp_args)))
+			else gp
+	
+	panel(observed, residuals=residuals, expected=expected, type=type,
+			residuals_type=residuals_type, gp=gp, ...)
+	
 }
 
