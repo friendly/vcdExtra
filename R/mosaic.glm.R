6f591c74a51f8823dcefa8a24dbb89c1f4b1d723
stsdan <stsdan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-02 16:39:47 +0000

    fixed bug so formula now works even when x$data unavailable
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vcdextra/pkg@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/mosaic.glm.R
+++ R/mosaic.glm.R
@@ -25,46 +25,47 @@
 	# But maybe not sensible for non-count data??
 	#if (!is.discrete.model(x)) stop("only factors are allowed")
 
-
 	df.residual <- x$df.residual
 	observed <- x$data
-	if (inherits(observed, "table"))
-            formula <- reformulate(names(dimnames(observed)))
-        else if (inherits(observed, "data.frame")) {
-            ## ok if equivalent to as.data.frame(xtab)
-            attr.terms <- attributes(terms(x))
-            resp <- deparse(attr.terms$variables[[2]])
-            factors <- setdiff(colnames(observed), resp)
-            ## assume everything bar response is an indexing factor (for now)
-            formula <- reformulate(factors)
-            ok <- TRUE
-            if (all(sapply(observed[factors], .MFclass) == "factor")) {
+        if (is.null(formula)) {
+            if (is.environment(observed)) observed <- model.frame(x)
+            if (inherits(observed, "table"))
+                formula <- reformulate(names(dimnames(observed)))
+            else if (inherits(observed, "data.frame")) {
+                ## ok if equivalent to as.data.frame(xtab)
+                attr.terms <- attributes(terms(x))
+                resp <- deparse(attr.terms$variables[[2]])
+                factors <- setdiff(colnames(observed), resp)
+                ## assume everything bar response is an indexing factor (for now)
+                formula <- reformulate(factors)
+                ok <- TRUE
                 ## check cross-classifying
                 per.cell <- tapply(observed[,1], observed[factors], length)
                 if (ok <- isTRUE(all(per.cell == 1))) {
                     warning("no formula provided, assuming ", deparse(formula),
                             "\n", call. = FALSE)
                 }
+                if (!ok)
+                    stop("cannot identify indexing factors from ", substitute(x),
+                         "$data - please provide formula", call. = FALSE)
             }
-            if (!ok)
-                stop("cannot identify indexing factors from ", substitute(x),
-                     "$data - please provide formula", call. = FALSE)
+            else
+                stop("cannot identify indexing factors - please provide formula",
+                     call. = FALSE)
         }
-        else
-            stop("cannot identify indexing factors - please provide formula",
-                 call. = FALSE)
-
+        if (length(formula) == 3) formula <- formula[-2]
         ## get indexing factors allowing for missing data, subset etc
 
-        factors <- model.frame(formula, data = observed, subset = x$call$subset)
+        factors <- do.call("model.frame", list(formula = formula, data = observed,
+                                               subset = x$call$subset))
         if (!is.null(x$na.action) && inherits(x$na.action, c("omit", "exclude")))
             factors <- factors[-x$na.action,]
 
-	# if a data.frame, extract the frequencies, re-shape as a table
-	if (inherits(observed, "data.frame"))
-            observed <- tapply(x$y, factors, I)
+	# if necessary create observed table
+	if (!is.table(observed))
+            observed <- as.table(tapply(x$y, factors, sum))
 
-	expected <- tapply(fitted(x), factors, I)
+	expected <- as.table(tapply(fitted(x), factors, sum))
 
 	type <- match.arg(tolower(type), c("observed", "expected"))
 	if (any(observed < 0, na.rm = TRUE))
@@ -75,7 +76,7 @@
 		residuals <- if (residuals_type=="rstandard") rstandard(x)
 				else residuals(x, type=residuals_type)
 	# reshape the residuals to conform to the structure of data
-	residuals <- tapply(residuals, factors, I)
+	residuals <- as.table(tapply(residuals, factors, sum))
 
 	gp <- if (inherits(gp, "grapcon_generator"))
 				do.call("gp", c(list(observed, residuals, expected, x$df.residual),
