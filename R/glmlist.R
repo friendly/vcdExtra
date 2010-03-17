76f66c543dd89a429a92cc849562fa3c65253ccd
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-17 16:29:41 +0000

    Added loglmlist, summarize.loglm
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vcdextra/pkg@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/glmlist.R
+++ R/glmlist.R
@@ -21,3 +21,39 @@ glmlist <- function(...) {
     return(args);
 }
 
+# loglmlist - do the same for loglm objects
+
+loglmlist <- function(...) {
+    args  <- list(...);
+    lname <- names(args)
+    name <- vname <- as.character(sys.call())[-1]
+    for (i in 1:length(args)) {
+        vname[i] <- if (length(lname) && lname[i] != "") 
+            lname[i]
+        else name[i]
+    }
+    names(args) <- vname[1:length(args)]
+    is.loglm <- unlist(lapply(args, function(x) inherits(x, "loglm")))
+    if (!all(is.loglm)) {
+    	warning("Objects ", paste(vname[!is.loglm], collapse=', '), 
+              " removed because they are not loglm objects")
+    	args <- args[is.loglm]
+    }
+    class(args) <- "loglmlist"
+    return(args);
+}
+
+# generic version: named list
+
+nlist <- function(...) {
+    args  <- list(...);
+    lname <- names(args)
+    name <- vname <- as.character(sys.call())[-1]
+    for (i in 1:length(args)) {
+        vname[i] <- if (length(lname) && lname[i] != "") 
+            lname[i]
+        else name[i]
+    }
+    names(args) <- vname[1:length(args)]
+    return(args);
+}
--- R/summarize.R
+++ R/summarize.R
@@ -5,36 +5,35 @@ summarize <- function(object, ...) {
 	UseMethod("summarize")
 }
 
-
-summarize.glm <-function(object, ..., dispersion=NULL, test=NULL){
-    dotargs <- list(...)
-    is.glm <- unlist(lapply(dotargs, function(x) inherits(x, 
-        "glm")))
-    dotargs <- dotargs[is.glm]
-    if (length(dotargs)) 
-        return(summarize.glmlist(c(list(object), dotargs), dispersion = dispersion, 
-            test = test))
-    # get object name if there is one
-	oname <- if (typeof(substitute(object))=="symbol") as.character(sys.call())[2] else NULL
-	
-    resdev <- object$deviance
-    resdf <- object$df.residual
-    p <- pchisq(resdev, resdf, lower.tail=FALSE)
-    aic <- resdev - 2*resdf
-    result <- data.frame(resdev, resdf, p, aic)
+stat.summarize <- function(deviance, df, onames) {
+    p <- pchisq(deviance, df, lower.tail=FALSE)
+    aic <- deviance - 2*df
+    result <- data.frame(deviance, df, p, aic)
     names(result) <- c("LR Chisq", "Df", "Pr(>Chisq)", "AIC")
-    rownames(result) <- oname
-    attr(result, "heading") <- c("Model Summary:")
+    rownames(result) <- onames
+    attr(result, "heading") <- "Model Summary:"
     class(result) <- c("anova", "data.frame")
     result
 }
 
-summarize.glmlist <-function(object, ..., dispersion=NULL, test=NULL){
+
+summarize.glm <-function(object, ..., test=NULL){
+    dotargs <- list(...)
+    is.glm <- unlist(lapply(dotargs, function(x) inherits(x, "glm")))
+    dotargs <- dotargs[is.glm]
+    if (length(dotargs)) 
+        return(summarize.glmlist(c(list(object), dotargs), test = test))
+    
+    oname <- as.character(sys.call())[2]
+    result <- stat.summarize(object$deviance, object$df.residual, oname)
+    result
+}
+
+summarize.glmlist <-function(object, ..., test=NULL){
     nmodels <- length(object)
     if (nmodels == 1) 
-        return(summarize.glm(object[[1]], dispersion = dispersion, 
-            test = test))
-	if (is.null(names(object))) {
+        return(summarize.glm(object[[1]], test = test))
+		if (is.null(names(object))) {
 	    oname <- as.character(sys.call())[-1]
 	    oname <- oname[1:length(object)]
 		}
@@ -42,13 +41,36 @@ summarize.glmlist <-function(object, ..., dispersion=NULL, test=NULL){
 
     resdf <- as.numeric(lapply(object, function(x) x$df.residual))
     resdev <- as.numeric(lapply(object, function(x) x$deviance))
-    p <- pchisq(resdev, resdf, lower.tail=FALSE)
-    aic <- resdev - 2*resdf
-    result <- data.frame(resdev, resdf, p, aic)
-    names(result) <- c("LR Chisq", "Df", "Pr(>Chisq)", "AIC")
-    rownames(result) <- oname
-    attr(result, "heading") <- c("Model Summary:")
-    class(result) <- c("anova", "data.frame")
+    result <- stat.summarize(resdev, resdf, oname)
+    result
+}
+
+
+summarize.loglm <-function(object, ...){
+    dotargs <- list(...)
+    is.loglm <- unlist(lapply(dotargs, function(x) inherits(x, "loglm")))
+    dotargs <- dotargs[is.loglm]
+    if (length(dotargs)) 
+        return(summarize.loglmlist(c(list(object), dotargs)))
+    
+    oname <- as.character(sys.call())[2]
+    result <- stat.summarize(object$deviance, object$df, oname)
+    result
+}
+
+summarize.loglmlist <-function(object, ...){
+    nmodels <- length(object)
+    if (nmodels == 1) 
+        return(summarize.loglm(object[[1]]))
+		if (is.null(names(object))) {
+	    oname <- as.character(sys.call())[-1]
+	    oname <- oname[1:length(object)]
+		}
+		else oname <- names(object)
+
+    resdf <- as.numeric(lapply(object, function(x) x$df))
+    resdev <- as.numeric(lapply(object, function(x) x$deviance))
+    result <- stat.summarize(resdev, resdf, oname)
     result
 }
 
