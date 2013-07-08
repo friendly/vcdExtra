ce199c3590b261b2c7edaf71aa989c9311c28c09
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-08 20:37:51 +0000

    added mosaic.loglmlist
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vcdextra/pkg@175 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/mosaic.glmlist.R
+++ R/mosaic.glmlist.R
@@ -2,6 +2,7 @@
 
 #' @param x    a glmlist object
 #' @param selection the index or name of one glm in \code{x}
+#' @param panel     panel function
 #' @param type      a character string indicating whether the \code{"observed"} or the \code{"expected"} values of the table should be visualized
 #' @param legend    show a legend in the mosaic displays?
 #' @param main      either a logical, or a vector of character strings used for plotting the main title. If main is a logical and TRUE, the name of the selected glm object  is used
@@ -13,31 +14,32 @@
 #' @export
 
 mosaic.glmlist <- function(x, selection,
-  type=c("observed", "expected"), 
-	legend=FALSE, main=NULL,
-	ask=TRUE, graphics=TRUE, rows, cols, newpage=TRUE,
-	 ...) {
-
-	calls <- sapply(x, mod.call)  # get model calls as strings
+		panel=mosaic,
+		type=c("observed", "expected"), 
+		legend=ask | !missing(selection),
+		main=NULL,
+		ask=TRUE, graphics=TRUE, rows, cols, newpage=TRUE,
+		...) {
+	
+#	calls <- sapply(x, mod.call)  # get model calls as strings
 	models <- names(x)
-  if (!is.null(main)) {
-      if (is.logical(main) && main) 
-          main <- models
-  }
-  else main <- rep(main, length(x))
- 
-
-  type=match.arg(type)
+	if (!is.null(main)) {
+		if (is.logical(main) && main) 
+			main <- models
+	}
+	else main <- rep(main, length(x))
+	
+	type=match.arg(type)
 	if (!missing(selection)){
-#		if (is.character(selection)) selection <- gsub(" ", "", selection)
-		return(mosaic(x[[selection]], type=type, main=main[selection], legend=legend, ...))
+		if (is.character(selection)) selection <- gsub(" ", "", selection)
+		return(panel(x[[selection]], type=type, main=main[selection], legend=legend, ...))
 	}
 	# perhaps make these model labels more explicit for the menu
 	if (ask & interactive()){
 		repeat {
 			selection <- menu(models, graphics=graphics, title="Select Model to Plot")
 			if (selection == 0) break
-			else mosaic(x[[selection]], type=type, main=models[selection], legend=legend, ...)
+			else panel(x[[selection]], type=type, main=main[selection], legend=legend, ...)
 		}
 	}
 	else {
@@ -47,22 +49,73 @@ mosaic.glmlist <- function(x, selection,
 			rows <- mfrow[1]
 			cols <- mfrow[2]
 		}
-
-    if (newpage) grid.newpage()
-    lay <- grid.layout(nrow=rows, ncol = cols)
+		
+		if (newpage) grid.newpage()
+		lay <- grid.layout(nrow=rows, ncol = cols)
 		pushViewport(viewport(layout = lay, y = 0, just = "bottom"))
 		for (i in 1:rows) {
 			for (j in 1:cols){
 				if ((sel <-(i-1)*cols + j) > nmodels) break
 				pushViewport(viewport(layout.pos.row=i, layout.pos.col=j))
-				mosaic(x[[sel]], type=type, main=models[sel], newpage=FALSE, legend=legend, ...)
+				panel(x[[sel]], type=type, main=main[sel], newpage=FALSE, legend=legend, ...)
 				popViewport()
 			}
 		}
 	}
-
 }
 
+mosaic.loglmlist <- function(x, selection,
+		panel=mosaic,
+		type=c("observed", "expected"), 
+		legend=ask | !missing(selection),
+		main=NULL,
+		ask=TRUE, graphics=TRUE, rows, cols, newpage=TRUE,
+		...) {
+	
+	models <- names(x)
+	strings <- as.vector(sapply(x, function(x) x$model.string))
+	if (!is.null(main)) {
+		if (is.logical(main) && main) 
+			main <- ifelse(as.vector(sapply(strings, is.null)), models, strings)
+	}
+	else main <- rep(main, length(x))
+	
+	type=match.arg(type)
+	if (!missing(selection)){
+		if (is.character(selection)) selection <- gsub(" ", "", selection)
+		return(panel(x[[selection]], type=type, main=main[selection], legend=legend, ...))
+	}
+	# perhaps make these model labels more explicit for the menu
+	if (ask & interactive()){
+		repeat {
+			selection <- menu(models, graphics=graphics, title="Select Model to Plot")
+			if (selection == 0) break
+			else panel(x[[selection]], type=type, main=main[selection], legend=legend, ...)
+		}
+	}
+	else {
+		nmodels <- length(x)
+		mfrow <- mfrow(nmodels)
+		if (missing(rows) || missing(cols)){
+			rows <- mfrow[1]
+			cols <- mfrow[2]
+		}
+		
+		if (newpage) grid.newpage()
+		lay <- grid.layout(nrow=rows, ncol = cols)
+		pushViewport(viewport(layout = lay, y = 0, just = "bottom"))
+		for (i in 1:rows) {
+			for (j in 1:cols){
+				if ((sel <-(i-1)*cols + j) > nmodels) break
+				pushViewport(viewport(layout.pos.row=i, layout.pos.col=j))
+				panel(x[[sel]], type=type, main=main[sel], newpage=FALSE, legend=legend, ...)
+				popViewport()
+			}
+		}
+	}
+}
+
+		
 # from effects::utilities.R
 mfrow <- function(n, max.plots=0){
 	# number of rows and columns for array of n plots
