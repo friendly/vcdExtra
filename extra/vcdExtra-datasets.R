96a1b4742097b335f53e06e8bf53fce6fda14029
Michael Friendly <friendly@yorku.ca>
2023-02-15 18:43:43 -0500

    classify datasets with \concept{}

--- extra/vcdExtra-datasets.R
+++ extra/vcdExtra-datasets.R
@@ -1,6 +1,34 @@
 # begin to classify vcdExtra datasets
 library(dplyr)
+library(tidyr)
 
 dsets <- datasets("vcdExtra") |> select("Item", "Title", "class", "dim")
 
 write.csv(dsets, file = "extra/vcdExtra-datasets.csv")
+
+# classify them in the equivalent xlsx file ...
+
+library(readxl)
+dsets_tagged <- read_excel("extra/vcdExtra-datasets.xlsx")
+
+dsets_tagged <- dsets_tagged |>
+  dplyr::select(-Title, -dim) |>
+  dplyr::rename(dataset = Item)
+str(dsets_tagged)
+
+# to invert the table, need to split tags into separate observations ???
+
+dset_split <- dsets_tagged |>
+  tidyr::separate_longer_delim(tags, delim = ";") |>
+  dplyr::mutate(tag = stringr::str_trim(tags)) |>
+  select(-tags)
+
+# collapse the rows for the same tag
+
+tag_dset <- dset_split |>
+  arrange(tag) |>
+  dplyr::group_by(tag, dataset) |>
+  dplyr::summarise(tag = paste(tag, collapse = ";"))
+
+
+  
