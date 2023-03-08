45f33970b05e0a315374d8bc3cfd840a6ced0db1
Michael Friendly <friendly@yorku.ca>
2023-03-08 12:51:14 -0500

    edit demo/occStatus.R

--- demo/occStatus.R
+++ demo/occStatus.R
@@ -6,26 +6,43 @@ library(vcdExtra)
 data(occupationalStatus)
 occupationalStatus
 
+# graphics::mosaicplot is the default plot method for a table
+plot(occupationalStatus, shade=TRUE)
+
 # define long labels for use in mosaics
 long.labels <- list(set_varnames = c(origin="origin: Father's status", 
                                      destination="destination: Son's status"))
 
-mosaic(occupationalStatus, shade=TRUE, main="Occupational status: Independence model", 
-    labeling_args = long.labels, legend=FALSE)
+mosaic(occupationalStatus, shade=TRUE, 
+       main="Occupational status: Independence model", 
+       labeling_args = long.labels, 
+       legend=FALSE)
+
+# the standard model of independence
+indep.glm <- glm(Freq ~ origin + destination, 
+                 family=poisson, 
+                 data=occupationalStatus)
+
+# the same mosaic, using the fitted model
+mosaic(indep.glm, 
+       main="Independence model",
+       labeling_args = long.labels, 
+       legend=FALSE, 
+       gp=shading_Friendly)
 
 
-indep.glm <- glm(Freq ~ origin + destination, family=poisson, data=occupationalStatus)
-mosaic(indep.glm, main="Independence model",
-  labeling_args = long.labels, legend=FALSE, gp=shading_Friendly)
-
-
-quasi.independence <-  gnm(Freq ~ origin + destination + Diag(origin,destination), family=poisson, data=occupationalStatus)
+# fit the model of quasi-independence, ignoring the diagonal cells
+quasi.independence <-  gnm(Freq ~ origin + destination + Diag(origin,destination), 
+                           family=poisson, data=occupationalStatus)
 str(quasi.independence$data)
 anova(quasi.independence)
 
 ## BUGLET (vcd): the diagonal cells should be considered 0 here--- outlined in black
-mosaic(occupationalStatus, expected=fitted(quasi.independence), main="Quasi-independence model",
-    labeling_args = long.labels, legend=FALSE, gp=shading_Friendly)
+mosaic(occupationalStatus, 
+       expected=fitted(quasi.independence), 
+       main="Quasi-independence model", 
+       labeling_args = long.labels, 
+       legend=FALSE, gp=shading_Friendly)
 
 
 ## using mosaic.gnm
@@ -33,17 +50,27 @@ mosaic(occupationalStatus, expected=fitted(quasi.independence), main="Quasi-inde
   labeling_args = long.labels, legend=FALSE, gp=shading_Friendly, labeling=labeling_residuals)
 
 
-symmetry <- glm(Freq ~ Symm(origin, destination), family=poisson, data=occupationalStatus)
+# symmetry model
+symmetry <- glm(Freq ~ Symm(origin, destination), 
+                family=poisson, 
+                data=occupationalStatus)
 mosaic(occupationalStatus, expected=fitted(symmetry), main="Symmetry model",
 	gp=shading_Friendly, labeling=labeling_residuals, labeling_args = long.labels )
 
 # using mosaic.glm --- OK
-mosaic(symmetry, main="Symmetry model",
-	gp=shading_Friendly, labeling=labeling_residuals, labeling_args = long.labels )
+mosaic(symmetry, 
+       main="Symmetry model", 
+       gp=shading_Friendly, 
+       labeling=labeling_residuals, 
+       labeling_args = long.labels )
 
-quasi.symm <- glm(Freq ~ origin + destination + Symm(origin, destination), family=poisson, data=occupationalStatus)
+quasi.symm <- glm(Freq ~ origin + destination + Symm(origin, destination), 
+                  family=poisson, 
+                  data=occupationalStatus)
 anova(quasi.symm)
-mosaic(occupationalStatus, expected=fitted(quasi.symm), main="Quasi-symmetry model")
+mosaic(occupationalStatus, 
+       expected=fitted(quasi.symm), 
+       main="Quasi-symmetry model")
 
 # model comparisons
 anova(independence, quasi.independence, quasi.symm, test="Chisq")
@@ -55,18 +82,38 @@ anova(symmetry, quasi.symm, test="Chisq")
 # uniform association, aka linear x linear association
 Rscore <- as.vector(row(occupationalStatus))
 Cscore <- as.vector(col(occupationalStatus))
-uniform <- gnm(Freq ~ origin + destination + Rscore:Cscore, 
-	family=poisson, data=occupationalStatus)
-mosaic(uniform, main="Uniform association model", labeling_args = long.labels, legend=FALSE,
-	gp=shading_Friendly, labeling=labeling_residuals )
 
-RChomog <- gnm(Freq ~ origin + destination + Diag(origin, destination) + MultHomog(origin, destination),
-	family=poisson, data=occupationalStatus)
-mosaic(RChomog, main="RC homogeneous model", labeling_args = long.labels, legend=FALSE,
-	gp=shading_Friendly, labeling=labeling_residuals)
+uniform <- gnm(Freq ~ origin + destination + Rscore:Cscore, 
+               family=poisson, 
+               data=occupationalStatus)
+
+mosaic(uniform, 
+       main="Uniform association model", 
+       labeling_args = long.labels, 
+       legend=FALSE,
+       gp=shading_Friendly, 
+       labeling=labeling_residuals )
+
+RChomog <- gnm(Freq ~ origin + destination + Diag(origin, destination) + 
+                 MultHomog(origin, destination), 
+               family=poisson, 
+               data=occupationalStatus)
+
+mosaic(RChomog, 
+       main="RC homogeneous model", 
+       labeling_args = long.labels, 
+       legend=FALSE,
+       gp=shading_Friendly, 
+       labeling=labeling_residuals)
 
 # RC1 - heterogeneous association
-RC1 <- gnm(Freq ~ origin + destination + Diag(origin, destination) + Mult(origin, destination),
-	family=poisson, data=occupationalStatus)
-mosaic(RC1, main="RC heterogeneous model", labeling_args = long.labels, legend=FALSE,
-	gp=shading_Friendly, labeling=labeling_residuals)
+RC1 <- gnm(Freq ~ origin + destination + Diag(origin, destination) + 
+             Mult(origin, destination),	
+           family=poisson, 
+           data=occupationalStatus)
+
+mosaic(RC1, main="RC heterogeneous model", 
+       labeling_args = long.labels, 
+       legend=FALSE,
+       gp=shading_Friendly, 
+       labeling=labeling_residuals)
