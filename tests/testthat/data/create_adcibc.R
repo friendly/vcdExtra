e6a1b335f3842dd25274ffe3e531a441b8097458
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2025-11-09 12:26:20 +0800

    correct data example

--- tests/testthat/data/create_adcibc.R
+++ tests/testthat/data/create_adcibc.R
@@ -1,11 +1,16 @@
 # The test data is from the CDISC Pilot data set,
 # which is publicly available on the PHUSE Test Data Factory repository.
 
+library(dplyr)
 url <- "https://github.com/phuse-org/TestDataFactory/raw/main/Updated/TDF_ADaM/adcibc.xpt"
 adcbic <- haven::read_xpt(url) %>%
   dplyr::filter(
-    EFFFL == 'Y' & ITTFL == 'Y',
-    AVISITN %in% c(8, 16, 24) & ANL01FL == 'Y'
+    EFFFL == "Y",
+    ITTFL == "Y",
+    AVISIT == "Week 8",
+    ANL01FL == "Y",
+    TRTPN != "54",
+    AGEGR1 != ">80"
   )
 write.csv(
   adcbic,
